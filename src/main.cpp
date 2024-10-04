23e31fe8dac18500beb1727e1c97002bcc75cae0
Zeroji <zzeroji@gmail.com>
2024-10-04 21:43:41 +0200

    Add ability to use serial monitor instead of chessboard

--- src/main.cpp
+++ src/main.cpp
@@ -12,24 +12,63 @@ U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C oled(U8G2_R0);
 
 Game game;
 
+uint64_t lastBoardState = DEFAULT_SENSORS_STATE;
+
 void setup() {
     initChessboard();
     lcd.begin(16, 2);
     oled.begin();
     Serial.begin(115200);
-    initializeGame(&game, 0x6600000000000066uLL);
+    initializeGame(&game, lastBoardState);
 }
 
 void loop() {
     const LCD_KEY keyPressed = getLastLcdKeyPressed();
 
+#ifdef USE_SERIAL_CHESSBOARD
+    uint64_t boardState = lastBoardState;
+
+    {
+        int c = Serial.read();
+        if ((c == '+' || c == '-') && Serial.available() >= 2) {
+            char buffer[2];
+            Serial.readBytes(buffer, 2);
+            uint8_t square = getSquareFromStr(buffer);
+            if (c == '+') {
+                boardState |= (1uLL << square);
+            } else {
+                boardState &= ~(1uLL << square);
+            }
+        }
+        if (c == '=' && Serial.available() >= 16) {
+            char hexBuffer[17];
+            if (16 == Serial.readBytes(hexBuffer, 16)) {
+                hexBuffer[16] = 0;
+                uint32_t low  = strtoul(&hexBuffer[8], nullptr, 16);
+                hexBuffer[8]  = 0;
+                uint32_t high = strtoul(hexBuffer, nullptr, 16);
+                boardState    = ((uint64_t)high << 32) | low;
+            }
+        }
+        if (c == 'Z') {
+            boardState = DEFAULT_SENSORS_STATE;
+            initializeGame(&game, boardState);
+        }
+    }
+#else
     const uint64_t boardState = readChessboard();
+#endif
 
     if (keyPressed == LCD_KEY::Select)
         initializeGame(&game, boardState);
 
     // Update game
-    evolveGame(&game, boardState);
+#ifdef USE_SERIAL_CHESSBOARD
+    if (boardState != lastBoardState)
+#endif
+    {
+        evolveGame(&game, boardState);
+    }
 
     // Display moves on LCD screen
     lcd.setCursor(0, 0);
@@ -59,4 +98,6 @@ void loop() {
                 if (boardState & (1uLL << ((7 - ly) * 8 + lx)))
                     oled.drawBox(lx * 8 + 32, ly * 4, 8, 4);
     } while (oled.nextPage());
+
+    lastBoardState = boardState;
 }
